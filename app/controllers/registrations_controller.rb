d27067b4040b63b7cb8c3fc82a9e7c9cf1b82410
Emilio <eivelez@miuandes.cl>
2019-04-20 18:49:14 -0400

    SignIn Done

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,9 +1,20 @@
 class RegistrationsController < ApplicationController
+
 	def new
-	
 	end
 
 	def create
-	    #complete this method
+		@registrations=User.new(parameters)
+		if @registrations.save
+			redirect_to root_url, notice: "Registration done"
+		else
+			flash[:error]="There was an error on your request"
+			render "new"
+		end
 	end
+	
+	def parameters
+		params.require(:registrations).permit(:name,:last_name, :email, :password, :phone)
+	end
+
 end
